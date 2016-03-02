582e0842756a8a5d91e82f39db7b609d8db72731
Krzysztof Antczak <antek116@gmail.com>
2016-03-02 13:31:27 -0800

    FrygaTime project finished.

--- FrygaTime/src/com/blstream/fryga/Main.java
+++ FrygaTime/src/com/blstream/fryga/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String [] args)
     {
-        FrygaClass frygaTime = new FrygaClass(args[0]);
+        FrygaClass frygaTime = new FrygaClass(args[0]); //Argument as time (hh:mm:ss)
         System.out.println(frygaTime.getFrygaTime());
     }
 }
